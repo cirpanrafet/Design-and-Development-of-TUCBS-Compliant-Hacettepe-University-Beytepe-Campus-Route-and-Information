f9b383a07a93df432597e0e28621d4aff4bb1275
RAFET <75682122+cirpanrafet@users.noreply.github.com>
2023-02-23 14:07:36 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const pool = new Pool({
     user: 'postgres',
-    password: '4399',
+    password: '****',
     host: 'localhost',
     port: 5432,
     database:'beytepe'
